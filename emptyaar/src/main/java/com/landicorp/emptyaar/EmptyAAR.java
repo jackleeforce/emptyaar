0b161b726d595e2c7b2f448b1c51a7b32027e642
lijiah <lijiah@landicorp.com>
2019-03-14 11:48:45 +0800

    first commit

---
+++ emptyaar/src/main/java/com/landicorp/emptyaar/EmptyAAR.java
@@ -0,0 +1,14 @@
+package com.landicorp.emptyaar;
+
+import timber.log.Timber;
+
+/**
+ * Author JackLee
+ */
+public class EmptyAAR {
+
+    public EmptyAAR() {
+
+        Timber.d("new EmptyAAR ");
+    }
+}
